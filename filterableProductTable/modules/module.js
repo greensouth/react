d2117c0f71ceb33e4d264a1a9a4870e2272a5b20
greensouth <foros@greensouth.net>
2022-10-16 23:23:09 +0200

    fin test

--- filterableProductTable/modules/module.js
+++ filterableProductTable/modules/module.js
@@ -31,45 +31,86 @@ class ProductCategory extends React.Component {
 class ProductTable extends React.Component {
     constructor(props) {
         super(props);
-        const tableDatat = props.data;
     }
+
     render(){
         let curCat = "";
         let rowItem = new Array();
-        let items = this.props.data.map( (el) => {              
-            if(curCat != el.category){
-                rowItem.push(<ProductCategory category = {el.category}/>);
-                curCat = el.category;
+        let items = this.props.data.map( (el) => {
+            if(this.props.filterText.trim().length == 0 || (this.props.filterText && el.name.toLowerCase().includes(this.props.filterText.toLowerCase()))){
+                if(!this.props.showStockOnly || (this.props.showStockOnly && el.stocked)){
+                    if(curCat != el.category){
+                        rowItem.push(<ProductCategory category = {el.category}/>);
+                        curCat = el.category;
+                    }
+                    rowItem.push(<ProductRow name={el.name} price={el.price} stocked={el.stocked}/>);
+                }
             }
-            if(!el.stocked){
-
-            }
-            rowItem.push(<ProductRow name={el.name} price={el.price} stocked={el.stocked}/>);
-
         } )        
-        return (<div><SearchBar/><table><tbody>{rowItem}</tbody></table></div>);
+        return (<table><tbody>{rowItem}</tbody></table>);
     }
 }
 
 class SearchBar extends React.Component {
     constructor(props){
         super(props);
+        this.onFilterChange = this.onFilterChange.bind(this);
+        this.onStockedChange = this.onStockedChange.bind(this);
+
+    }
+    onFilterChange(e){
+        this.props.onSearchChange(e.target.value);
+    }
+
+    onStockedChange(e){
+        this.props.onStockedChange(e.target.checked);
     }
     render(){
         const value = !this.props.inputValue ? "" : this.props.value
         const checked = this.props.checked ?? false;
-        let checkbox
+        let checkbox;
         if(checked){
-            checkbox = <input type="checkbox" checked></input>
+            checkbox = <input type="checkbox" checked onChange={this.onStockedChange}></input>
         }else{
-            checkbox = <input type="checkbox"></input>
+            checkbox = <input type="checkbox" onChange={this.onStockedChange}></input>
         }
         return (
             <form>
-                <input id="search" type="text" placeholder="Search..." value={value}></input>
+                <input id="search" type="text" placeholder="Search..." onChange={this.onFilterChange}></input>
                 <label>
                     {checkbox} Only show in stock</label>
             </form>
             );
+    }
+}
+
+class FilterableProductTable extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            filterText: "",
+            inStockOnly: false
+        }
+        this.onSearchChange = this.onSearchChange.bind(this)
+        this.onStockedChange = this.onStockedChange.bind(this)
+    }
+    onSearchChange(searchValue){
+        this.setState((state,props) => ({
+            filterText: searchValue
+        }))
+    }
+    onStockedChange(stockedValue){
+        this.setState((state,props) => ({
+            inStockOnly: stockedValue
+        }))
+    }
+    
+    render(){
+        return (
+            <div>
+                <SearchBar filterText={this.state.filterText} showStockOnly={this.state.inStockOnly} onStockedChange={this.onStockedChange} onSearchChange={this.onSearchChange}/>
+                <ProductTable filterText={this.state.filterText} showStockOnly={this.state.inStockOnly} data = {this.props.data}/>
+            </div>
+        );
     }
 }
