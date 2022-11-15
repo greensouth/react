1f718eff767ac2c8cc598e1715b0c612abd86acd
Alex Fernández <alex@greensouth.net>
2022-11-15 18:37:45 +0100

    safas
    fasf

--- v16/src/App.js
+++ v16/src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
-
+import './components/card.jsx';
 function App() {
+
+  const dataGames = [
+    {
+      name:   "Sweet Bonanza",
+      url:    "https://www.lottofy.com/slots/sweet-bonanza/",
+      thumb:  "//cdn.inicium.com/images/lottofy/games/medium/166.jpg"
+    }
+  ]
   return (
     <div className="App">
-
+      <h1>holi!</h1>
+      <Card name={dataGames[0].name} thumb={dataGames[0].thumb} url={dataGames[0].url}/>
     </div>
   );
 }
--- v16/src/components/card.jsx
+++ v16/src/components/card.jsx
@@ -1,6 +1,12 @@
-function card(){
-
+export function Card({props}){
+    const doubleHTML = props.double ? "double" : "";
+    console.log(doubleHTML);
     return (
-        <li className="miniCard"></li>
+        <li className="miniCard {doubleHTML}">
+            <span data-gameName="{name}"></span>
+            <a data-gameitem href="{url}" title="{name}">
+                <img src="{thumb}" alt=""></img>
+            </a>
+        </li>
     );
 }
